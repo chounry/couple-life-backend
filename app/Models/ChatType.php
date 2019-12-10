8dda243bb2205372b640c801085909bf8a8a3197
chounry <oun.chounry.kh@gmail.com>
2019-12-10 14:27:53 +0700

    add : table, model

---
+++ app/Models/ChatType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ChatType extends Model
+{
+    // public $timestamps = false;
+    // protected $table = 'estates';
+    protected $fillable = [];
+}
---
+++ app/Models/MediaType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MediaType extends Model
+{
+    // public $timestamps = false;
+    // protected $table = 'estates';
+    protected $fillable = [];
+}
