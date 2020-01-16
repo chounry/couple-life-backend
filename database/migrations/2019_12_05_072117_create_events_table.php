4988b15dc75da4bdd0b518a549934d3269691f3f
chounry <oun.chounry.kh@gmail.com>
2020-01-16 14:42:00 +0700

    update : table field

--- database/migrations/2019_12_05_072117_create_events_table.php
+++ database/migrations/2019_12_05_072117_create_events_table.php
@@ -15,6 +15,7 @@ class CreateEventsTable extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title');
             $table->string('description')->nullable();
             $table->string('lat');
             $table->string('lng');
